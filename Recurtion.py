96f902de719cfa175de23348fb7270798a1e0a6a
fletch <fletch4503@gmail.com>
2022-12-07 21:16:52 +0300

    Recurtion

--- Recurtion.py
+++ Recurtion.py
@@ -3,15 +3,25 @@
 
 import time
 def fact(n):
-    if n == 1:
+    if n < 0:
+      raise ValueError
+    if n == 0:
        return 1
     return fact(n - 1) * n
 def fact_iter(n):
+    if n < 0:
+        raise ValueError
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
-# fact(500),
+def fact_tail(n, acc=1):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+     return acc
+    return fact_tail(n - 1, acc*n)
+
 if __name__ == '__main__':
     print(fact(5))
 #    print(fact_iter(5))
@@ -19,8 +29,14 @@ if __name__ == '__main__':
 #    print(fact_iter(5))
     timing = []
     start = time.time()
-    for test_value in range(500):
+    for test_value in range(5000):
         fact(test_value)
-        end = time.time()
-        timing.append(end - start)
-        start = end
+    end = time.time()
+    timing.append(end - start)
+    start = end
+    for test_value in range(5000):
+        fact_iter(test_value)
+    end = time.time()
+    timing.append(end - start)
+    start = end
+    print(timing)
