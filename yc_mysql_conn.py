01e9d68f62de2bd20b88152ccc3e8cc715474f9b
fletchMac <fletch4503@gmail.com>
2023-01-28 21:59:08 +0300

    Tracing SELECT operation with db + Structured output of results

--- yc_mysql_conn.py
+++ yc_mysql_conn.py
@@ -1,7 +1,6 @@
 """
 Connect to database on the host 37.46.131.241
 """
-
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -9,17 +8,17 @@ Connect to database on the host 37.46.131.241
 import mysql.connector
 
 conn = mysql.connector.connect(
-      host="37.46.131.241",
-      port=3306,
-      db="yc_project",
-      user="fletch4503",
-      passwd="Manta_50174416")
+    host="37.46.131.241",
+    port=3306,
+    db="yc_project",
+    user="fletch4503",
+    passwd="Manta_50174416")
 #      ssl={'ca': '~/.mysql/root.crt'})
 cur = conn.cursor()
 cur.execute("SELECT * FROM mtst WHERE rg_rg_id = 3;")
 rows = cur.fetchall()
 for row in rows:
-      print('{0} {1} {2} {3} {4} {5} {6}'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+    print(f'{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]}')
 # m_list = []
 # cur = conn.cursor()
 # # cur.execute('SELECT version()')
