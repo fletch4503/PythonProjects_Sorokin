481035d7a54dd4373d5bf749f8050c9ace3aeede
fletchMac <fletch4503@gmail.com>
2023-01-28 21:49:21 +0300

    Tracing SELECT operation with db + Structured output of results

--- yc_mysql_conn.py
+++ yc_mysql_conn.py
@@ -15,13 +15,16 @@ conn = mysql.connector.connect(
       user="fletch4503",
       passwd="Manta_50174416")
 #      ssl={'ca': '~/.mysql/root.crt'})
-
-m_list = []
 cur = conn.cursor()
-# cur.execute('SELECT version()')
-# print(cur.fetchone()[0])
-mtstlist = cur.execute('SELECT MTST_surname FROM mtst WHERE rg_rg_id = 1;')
-for result in mtstlist:    m_list.append(result[0])
-print(m_list)
-
+cur.execute("SELECT * FROM mtst WHERE rg_rg_id = 3;")
+rows = cur.fetchall()
+for row in rows:
+      print('{0} {1} {2} {3} {4} {5} {6}'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+# m_list = []
+# cur = conn.cursor()
+# # cur.execute('SELECT version()')
+# # print(cur.fetchone()[0])
+# mtstlist = cur.execute('SELECT MTST_surname FROM mtst WHERE rg_rg_id = 1;')
+# for result in mtstlist:    m_list.append(result[0])
+# print(m_list)
 conn.close()
