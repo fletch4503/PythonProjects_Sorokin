182d0336499d35bad64860e7d54205cb4ea5c5b4
fletchGitHub <fletch4503@gmail.com>
2023-01-28 18:59:13 +0300

    Need to install mysql.connector module

--- yc_mysql_conn.py
+++ yc_mysql_conn.py
@@ -1,6 +1,7 @@
 """
 Connect to database on the host 37.46.131.241
 """
+
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -15,9 +16,12 @@ conn = mysql.connector.connect(
       passwd="Manta_50174416")
 #      ssl={'ca': '~/.mysql/root.crt'})
 
+m_list = []
 cur = conn.cursor()
-cur.execute('SELECT version()')
-
-print(cur.fetchone()[0])
+# cur.execute('SELECT version()')
+# print(cur.fetchone()[0])
+mtstlist = cur.execute('SELECT MTST_surname FROM mtst WHERE rg_rg_id = 1;')
+for result in mtstlist:    m_list.append(result[0])
+print(m_list)
 
 conn.close()
